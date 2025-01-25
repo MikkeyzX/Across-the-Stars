0bed06aee7db6c31e14ffe91719f5a17b7fee078
Iwolic32 <145413137+Iwolic32@users.noreply.github.com>
2025-01-25 03:16:08 +0100

    Nothing really changed

--- Assets/Player_FlashlightMovement.cs
+++ Assets/Player_FlashlightMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Camera : MonoBehaviour
+public class Player_FlashlightMovement : MonoBehaviour
 {
     public Transform orientation;
 
@@ -19,15 +19,13 @@ public class Player_Camera : MonoBehaviour
 
     private void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-            
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX * 20000;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY * 20000;
+
         yRotation += mouseX;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, xRotation); 
     }
 }
